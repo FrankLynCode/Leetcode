480f6f6a8eabeb1ee83f30266d6f3781dd21ddfe
FrankCode <357015691@qq.com>
2019-11-05 01:55:07 -0500

    try lambda

--- src/FindWordByChars.kt
+++ src/FindWordByChars.kt
@@ -1,22 +1,48 @@
 fun main(){
-    print( countCharacters(arrayOf("atd","sfw"),"whwat"))
+//    println( countCharacters(arrayOf("atd","sfw"),"whwat"))
+//    println(countCharacters)
+    println(test())
+}
+
+/**
+ * Solution 1
+ */
+//fun countCharacters(words: Array<String>, chars: String): Int {
+//
+//    val frequencyMap =  chars.groupingBy { it }.eachCount()
+//    var count = 0
+//    words.forEach unvalidword@ {word ->
+//        val frequencyCharact = word.groupingBy { it }.eachCount()
+//        frequencyCharact.forEach { charactCount ->
+//            if (!( frequencyMap.containsKey(charactCount.key) &&
+//                        frequencyMap.getOrDefault(charactCount.key,0) >= charactCount.value)){
+//                return@unvalidword
+//            }
+//        }
+//        count += word.length
+//    }
+//
+//return count
+//}
+    /**
+     * Solution 2
+     */
+fun countCharacters(words: Array<String>, chars: String): Int {
+    fun good(s: String, t: String): Boolean = ('a'..'z').all { ch ->
+        s.count { it == ch } <= t.count {it == ch}
+    }
+    return words.sumBy { if (good(it, chars)) it.length else 0 }
 }
 
 
-fun countCharacters(words: Array<String>, chars: String): Int {
+val zeroToTen = 0..10
 
-    val frequencyMap =  chars.groupingBy { it }.eachCount()
-    var count = 0
-    words.forEach unvalidword@ {word ->
-        val frequencyCharact = word.groupingBy { it }.eachCount()
-        frequencyCharact.forEach { charactCount ->
-            if (!( frequencyMap.containsKey(charactCount.key) &&
-                        frequencyMap.getOrDefault(charactCount.key,0) >= charactCount.value)){
-                return@unvalidword
-            }
-        }
-        count += word.length
-    }
+
+fun test() :Boolean = ('a'..'z').all {
+    println(it)
+    it in 'a'..'z'
+
+}
 
     /**
      * Test
@@ -26,6 +52,3 @@ fun countCharacters(words: Array<String>, chars: String): Int {
 //    val keyList = ArrayList(frequencyMap.keys)
 //    println(keyList)
 
-
-    return count
-}
