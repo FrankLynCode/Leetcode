f2cbcea48179e21fc223c3a32b9ba9cb5234230e
FrankCode <357015691@qq.com>
2019-12-18 18:29:02 -0500

    almost nothing changed

--- src/IncreseAndCountInMatrix.kt
+++ src/IncreseAndCountInMatrix.kt
@@ -10,18 +10,21 @@ The final matrix will be [[1,3,1],[1,3,1]] which contains 6 odd numbers.
  */
 fun main(args: Array<String>) {
     println(oddCells(2, 3, arrayOf(intArrayOf(0, 1), intArrayOf(1, 1))))
+
+
 }
+
+
 
 fun oddCells(n: Int, m: Int, indices: Array<IntArray>): Int {
     var result = 0
-    var matrix = Array(n,init = {IntArray(m,init = {0})})
-    indices.forEach { cell->
-        matrix[cell[0]].forEachIndexed { index, _ ->
-            ++matrix[cell[0]][index]
+    val matrix = Array(n,init = {IntArray(m,init = {0})})
+    indices.forEach { pair->
+        matrix[pair[0]].forEachIndexed { index, cell ->
+            matrix[pair[0]][index]++
         }
-
-        matrix.forEachIndexed { index, _ ->
-            ++matrix[index][cell[1]]
+        matrix.forEach {
+            it[pair[1]]++
         }
     }
     matrix.forEach { row ->
